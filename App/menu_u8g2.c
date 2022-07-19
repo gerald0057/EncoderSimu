f556548976c15a89356489287125e06008b15b65
gerald0057 <1565179402@qq.com>
2022-07-19 18:58:35 +0800

    menu glue code implement

--- App/menu_u8g2.c
+++ App/menu_u8g2.c
@@ -15,3 +15,66 @@
  */
 
 #include "menu_u8g2.h"
+#include "u8g2.h"
+#include <string.h>
+
+struct u8g2_api
+{
+    u8g2_t *u;
+    int color;
+};
+
+static struct u8g2_api uapi;
+
+int menu_api_register_handle(void *handle)
+{
+    if (NULL == handle)
+    {
+        return -1;
+    }
+
+    uapi.u = (u8g2_t *)handle;
+
+    return 0;
+}
+
+void menu_api_draw_box(int x, int y, int w, int h)
+{
+    u8g2_DrawBox(uapi.u, x, y, w, h);
+}
+
+void menu_api_draw_frame(int x, int y, int w, int h)
+{
+    u8g2_DrawFrame(uapi.u, x, y, w, h);
+}
+
+void menu_api_draw_str(int x, int y, const char *str)
+{
+    u8g2_DrawStr(uapi.u, x, y, str);
+}
+
+void menu_api_update_screen(void)
+{
+    u8g2_SendBuffer(uapi.u);
+}
+
+void menu_api_set_draw_color(int color)
+{
+    uapi.color = color;
+    u8g2_SetDrawColor(uapi.u, uapi.color);
+}
+
+void menu_api_clear_buf(void)
+{
+    u8g2_ClearBuffer(uapi.u);
+}
+
+int menu_api_set_font(const char *fontname)
+{
+    if (!strcmp(fontname, "u8g2_font_t0_11b_tf"))
+    {
+        u8g2_SetFont(uapi.u, u8g2_font_t0_11b_tf);
+    }
+
+    return 0;
+}
--- App/menu_u8g2.h
+++ App/menu_u8g2.h
@@ -21,7 +21,14 @@
 extern "C" {
 #endif
 
-
+int menu_api_register_handle(void *handle);
+void menu_api_draw_box(int x, int y, int w, int h);
+void menu_api_draw_frame(int x, int y, int w, int h);
+void menu_api_draw_str(int x, int y, const char *str);
+void menu_api_update_screen(void);
+void menu_api_set_draw_color(int color);
+int menu_api_set_font(const char *fontname);
+void menu_api_clear_buf(void);
 
 #ifdef __cplusplus
 }
